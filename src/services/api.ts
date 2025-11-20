cedca1425c20d25d9bd7879f9471359404776ba4
Rogério Fernandes <roferrock@gmail.com>
2025-11-20 00:39:33 -0300

    Finalizing the deployment and testing in production

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://10.163.34.166:3333'
-    //baseURL: 'https://web-production-a5f94.up.railway.app'
+    //baseURL: 'http://10.163.34.166:3333', // Desenvolvimento local
+    baseURL: 'https://web-production-a5f94.up.railway.app', // Produção
+    timeout: 10000 // 10 segundos
 });
 
 export {api};
